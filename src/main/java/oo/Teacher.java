c842458f12d78c21e1bc49585e65b7031ced998d
VSHI <36368406@QQ.COM>
2024-11-21 22:11:37 +0800

    Feat: add codes for step 4

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -1,14 +1,37 @@
 package oo;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Teacher extends Person {
 
     public static final String TEACHER_ROLE = "teacher";
+    private final Set<Klass> klasses = new HashSet<>();
 
     public Teacher(int id, String name, int age){
         super(id,name,age, TEACHER_ROLE);
     }
 
     public String introduce(){
-        return super.introduce() + " I am a "+ TEACHER_ROLE + ".";
+        String classNumbers = klasses.stream()
+                .map(Klass::getClassNumber)
+                .map(String::valueOf)
+                .collect(Collectors.joining(", "));
+
+        return super.introduce() + " I am a "+ TEACHER_ROLE + "." +(classNumbers.isEmpty()?"":" I teach Class "+classNumbers+".");
+    }
+
+    public void assignTo(Klass klassToAssign){
+        klasses.add(klassToAssign);
+    }
+
+    public boolean belongsTo(Klass klassToCheck){
+        return klasses.contains(klassToCheck);
+    }
+
+    public boolean isTeaching(Student studentToCheck){
+        return klasses.contains(studentToCheck.klass);
     }
 }
