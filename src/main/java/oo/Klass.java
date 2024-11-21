6a6d454cf5d996b774b46e3c345afa02c4e2e6c3
VSHI <36368406@QQ.COM>
2024-11-21 21:59:58 +0800

    Feat: add codes for step 3

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -1,4 +1,28 @@
 package oo;
 
+import java.util.Objects;
+
 public class Klass {
+
+    public int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return number == klass.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(number);
+    }
+
+    public int getClassNumber(){
+        return number;
+    }
 }
--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -3,12 +3,21 @@ package oo;
 public class Student extends Person {
 
     public static final String STUDENT_ROLE = "student";
+    public Klass klass;
 
     public Student (int id, String name, int age){
         super(id,name,age, STUDENT_ROLE);
     }
 
     public String introduce(){
-        return super.introduce() + " I am a "+ STUDENT_ROLE + ".";
+        return super.introduce() + " I am a "+ STUDENT_ROLE + "." + (klass == null?"":" I am in class "+klass.getClassNumber() + ".");
+    }
+
+    public void join(Klass klassToJoin){
+        this.klass = klassToJoin;
+    }
+
+    public boolean isIn(Klass klassToCheck){
+            return this.klass != null && this.klass.equals(klassToCheck);
     }
 }
